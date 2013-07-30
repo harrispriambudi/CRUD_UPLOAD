653557874e1dc9a239948ce554396e11255a594b
harris priambudi <hrrzorcozzy@gmail.com>
2013-07-30 16:45:24 +0700

    CRUD and UPLOAD + Transaction ( delete )
    implementasi metode transaction pada delete

--- delete.php
+++ delete.php
@@ -1,22 +1,37 @@
 <?php
-require_once 'include/db.php';
-
 if( isset($_GET['id']))
 {
-	$sql = $db->prepare('SELECT * FROM crud WHERE id = :id');
-	$sql->bindValue(':id', $id, PDO::PARAM_INT);
-	$sql->execute();
-	$results = $sql->fetch(PDO::FETCH_OBJ);
-	$x = $results->x;
+	try{
+ $id=$_GET['id'];
+ $db = new PDO('mysql:host=localhost;dbname=simple;charset=UTF-8', 'root', '');
+ $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+ $db->beginTransaction();
+ $query_get = "SELECT * FROM crud WHERE id=$id";
+ $stmt = $db->prepare($query_get);
+ $stmt->execute();
+ $results = $stmt->fetchAll(PDO::FETCH_OBJ);
+ $query_delete="delete from crud where id= $id";
+ $stmt = $db->prepare($query_delete);
+ $stmt->execute();
+ $db->commit();
+ }
+ catch(PDOException $e){
+ $db->rollBack();
+ echo $e->getMessage();
+ }
+ }
+?>
 
-	//unlink("assets/foto_akun/$item->foto_karyawan");
-
-	$sql = $db->prepare('DELETE FROM crud WHERE id = :id');
-	$sql->bindValue(':id', $_GET['id'], PDO::PARAM_INT);
-	$sql->execute();
+ <?php foreach($results as $row): ?>
+ <tr>
+ <td><?php echo $row->x ?></td>
+ <td><?php echo $row->y ?></td>
+ <td><?php echo $row->z ?></td>
+ </tr>
+ Telah dihapus
+ <?php endforeach;
+ $stmt->closeCursor();
+ $dbh=null;
+ ?>
 	
 	
-}
-
-	header('Location: home.php');
-	exit;
